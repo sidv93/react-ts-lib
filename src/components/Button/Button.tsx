88047a8db461dac5d16013dc83920d82dd6707a6
Siddharth Venkatesh <siddhuv93@gmail.com>
2021-08-16 00:22:33 +0530

    Added ts and rollup config

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { FC } from 'react';
 
 import './Button.css';
 import { ButtonProps } from './Button.types';
 
-const Button = ({ type, text, onClick }: ButtonProps) => {
+const Button: FC<ButtonProps>  = ({ type, text, onClick }) => {
     return (
         <button type="button" className={`Button Button-${type}`} onClick={onClick}>
             {text}
